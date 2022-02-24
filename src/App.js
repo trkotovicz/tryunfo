c85fa75a8edeab6d3b4f986b5fd0cf0f9c9cfa76
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-02-24 00:24:04 -0300

    Requisito 4 em andamento

--- src/App.js
+++ src/App.js
@@ -3,12 +3,58 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+      description: '',
+      attr1: 0,
+      attr2: 0,
+      attr3: 0,
+      image: '',
+      rarity: normal,
+      isTrunfo: false,
+      hasTrunfo: false,
+    };
+  }
+
+  handleChange() {
+
+  }
+
   render() {
+    const { name, description, attr1,
+      attr2, attr3, image, rarity,
+      isTrunfo, hasTrunfo } = this.state;
+
     return (
       <>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rarity }
+          cardTrunfo={ isTrunfo }
+          hasTrunfo={ hasTrunfo }
+        />
+        <Card
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rarity }
+          cardTrunfo={ isTrunfo }
+          hasTrunfo={ hasTrunfo }
+        />
       </>
     );
   }
